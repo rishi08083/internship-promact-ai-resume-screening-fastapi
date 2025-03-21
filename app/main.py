da9da7245d70edbea6128e6ed7ebf7a0db5cd177
Rohit Ezhuthachan <ezurohit2003@gmail.com>
2025-03-21 17:23:37 +0530

    Add .doc extraction support and update CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
+<<<<<<< Updated upstream
 from api import resume
 
 # from app import resume, screening
 
 app = FastAPI()
 
+=======
+from fastapi.middleware.cors import CORSMiddleware
+from api import resume
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+>>>>>>> Stashed changes
 app.include_router(resume.router, prefix="/api", tags=["resume"])
 # app.include_router(screening.router, prefix="/api/v1", tags=["screening"])
+
 
 @app.get("/")
 def health_check():
